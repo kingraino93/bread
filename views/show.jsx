70eabe1a83a496742709bed76eb4d5ce972261f5
kingraino93 <76224373+kingraino93@users.noreply.github.com>
2022-08-21 13:30:36 -0400

    Sunday

--- views/show.jsx
+++ views/show.jsx
@@ -7,12 +7,16 @@ function Show({ bread, index }) {
     return (
         <Default>
             <h3>{bread.name}</h3>
-            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+            <img src={bread.image} alt={bread.name} />
+            <p>Baked by {bread.baker}</p>
+            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
 
 
-            <form action={`/breads/${index}?_method=DELETE`} method="POST">
+
+            <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE" />
             </form>
+
 
             <p>
                 and it
